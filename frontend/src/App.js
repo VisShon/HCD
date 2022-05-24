fd67494e76a209d774679228e8f94d6bd4f7c918
VisShon <vishnu20414@iiitd.ac.in>
2022-05-24 22:01:52 +0530

    navs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,17 @@ import logo from './assets/logo.png';
 import dropN from './assets/dropN.svg';
 import './styles/global.css';
 import useState from 'react';
+import NavBar from './components/navbar';
 
 function App(props) {
   // const cardContent = useState(props.cardContent);
   return (
     <>
       <div className="relative">
-
+        <NavBar/>
         <div id='topCard' className='card z-10 absolute'>
           <img id='logoImg' src={logo}/>
         </div> 
-
         <div id='bottomCard' className='card z-5 absolute bottom-0 right-0' >
           <div id='cardHead' className='font-Archivo text-[#424949]'>
               {props.cardContent.heading}
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,23 @@
 export default function NavBar(){
     return(
         <>
+            <div id='navbar' className='font-Maven text-[#444343]'>
+                <li>
+                    <div to='/'>ABOUT</div>
+                </li>
+                <li>
+                    <div to='/'>ACADEMICS</div>
+                </li>
+                <li>
+                    <div to='/'>PEOPLE</div>
+                </li>
+                <li>
+                    <div to='/'>RESEARCH</div>
+                </li>
+                <li>
+                    <div to='/'>CONTACT US</div>
+                </li>
+            </div>
         </>
     )
 }
