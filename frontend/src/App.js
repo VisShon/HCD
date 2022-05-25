7598703c5fd7fd53389337cc86fa7672a93ccce2
VisShon <vishnu20414@iiitd.ac.in>
2022-05-25 22:39:36 +0530

    hey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,32 @@ import NavBar from './components/navbar';
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
 import BottomCard from './components/bottomCard';
 import TopCard from './components/topCard';
-
+const FadeUp = batch(Fade(), Move(), Sticky());
 function App(data) {
   // const cardContent = useState(data.cardContent);
   return (
     <>
-      <div className="relative">
+      <ScrollContainer>
 
+          <NavBar/>
 
-        <NavBar/>
-        <TopCard />
-        <BottomCard cardContent={data.cardContent}/>
-
-        <video loop autoPlay muted id='bgrVideo' className='h-screen w-screen object-cover'>
-          <source src={video} type="video/mp4"/>Your browser does not support the video tag.
-        </video> 
-
-        <ScrollContainer>
-          <ScrollPage>
-
-              <Animator animation={MoveOut(0, -200)}>
-                <div id='whyUs_Page'> 
-                  <div className='font-Maven font-semibold text-[#39AEA8] text-[10vw]'>WHY US?</div>
-                </div>
+          <ScrollPage page={0}>
+              <TopCard />
+              <BottomCard cardContent={data.cardContent}/>
+              <video loop autoPlay muted id='bgrVideo' className='h-screen w-screen object-cover'>
+                <source src={video} type="video/mp4"/>Your browser does not support the video tag.
+              </video> 
+          </ScrollPage>
+          
+          <ScrollPage page={1}>
+            <div id='whyUs_Page'> 
+              <Animator animation={MoveIn(1000,0)}>
+                <div className='font-Maven font-semibold text-[#39AEA8] text-[10vw]'>WHY US?</div>
               </Animator>
-
+            </div>
           </ScrollPage>
 
-        </ScrollContainer>
-
-      </div>
+      </ScrollContainer>
     </>
   );
 }
