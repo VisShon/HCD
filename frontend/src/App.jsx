4b159d8a1de51076c5aa4c74f8e7cd075cc3921f
Ambujbhaskar <bhaskarambuj7@gmail.com>
2022-05-25 23:58:39 +0530

    fixed alumni text margin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App(data) {
               <Animator animation={MoveAlumn}>
                 <img src={alumn}  className='h-screen w-screen object-cover z-0' />
               </Animator>
-                <div id='AlumniTXT' className='absolute z-10 font-Archivo text-[#ffffff] text-[10vw]'>ALUMNI</div>
+                <div id='AlumniHeading' className='absolute z-10 font-Archivo text-[#ffffff] text-[10vw]'>ALUMNI</div>
             </div>
           </ScrollPage>
 
