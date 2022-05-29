8bb9ae0888fc0f358a6aac4f567b3e56462bf767
VisShon <vishnu20414@iiitd.ac.in>
2022-05-30 03:01:09 +0530

    coming soon

---
+++ frontend/src/components/navbar2.jsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom";
+import '../styles/navbar.css';
+import logo from '../assets/logo.png';
+
+export default function NavBar2(){
+    return(
+        <>  
+            <div id='navbar2' className="flex items-center">
+                <div className="rounded-br-[90px] bg-[#F1F1F1] items-center">
+                    <img src = {logo} className="h-[10em] mx-[2vw] my-[1vw]"/>
+                </div>
+                <div id='navbar' className='font-Archivo text-[#444343] '>
+                    <Link to='about'>ABOUT</Link>
+                    <div to='/'>|</div>
+                    <Link to='academics'>ACADEMICS</Link>
+                    <div to='/'>|</div>
+                    <Link to='people'>PEOPLE</Link>
+                    <div to='/'>|</div>
+                    <Link to='research'>RESEARCH</Link>
+                    <div to='/'>|</div>
+                    <Link to='contactus'>CONTACT US</Link>
+                </div>
+            </div>
+        </>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 import Landing from './pages/Landing';
+import CommingSoon from './pages/Commingsoon';
+import NotFound from './pages/Notfound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Landing/>} />
+    </Routes>
+    <Routes>
+      <Route path="/CommingSoon" element={<CommingSoon/>}/>
+    </Routes>
+    <Routes>
+      <Route path="/NotFound" element={<NotFound/>}/>
     </Routes>
   </BrowserRouter>
 );
