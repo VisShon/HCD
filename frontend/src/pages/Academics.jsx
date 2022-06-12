4e8f28177920740c1f6651e8014be2f469f23816
Ambujbhaskar <bhaskarambuj7@gmail.com>
2022-06-12 18:34:10 +0530

    section 4 acad page

--- frontend/src/pages/Academics.jsx
+++ frontend/src/pages/Academics.jsx
@@ -4,6 +4,7 @@ import "../styles/academics.css";
 import Section1 from "../components/Academics/section1";
 import Section2 from "../components/Academics/section2";
 import Section3 from "../components/Academics/section3";
+import Section4 from "../components/Academics/section4";
 
 export default function Academics(){
     return(
@@ -12,6 +13,7 @@ export default function Academics(){
             <Section1 data={data.section1} />
             <Section2 data={data.section2}/>
             <Section3 data={data.section3} />
+            <Section4 data={data.section4} />
         </>
     );
 }
