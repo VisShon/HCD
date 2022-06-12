3d999e7273cdbf1a74877432e360e285477178ed
Ambujbhaskar <bhaskarambuj7@gmail.com>
2022-06-13 00:24:53 +0530

    made section 7 more responsive

--- frontend/src/components/Academics/section7.jsx
+++ frontend/src/components/Academics/section7.jsx
@@ -4,23 +4,21 @@ import NavBar from "../navbar";
 export default function Section7({data}){
     return(
         <>
-        <NavBar logo={true} />
+            <NavBar logo={true} />
             <div className="section7Container">
                 <h1>
                     {data.heading}
                 </h1>
                 <div className="cardsContainer">
                     {
-                        data.policies.map((policy) => {
-                            return (
-                                <a href={policy.link}>
-                                    <button>
-                                        <p>{policy.text}</p>
-                                        <img src={arrow} />
-                                    </button>
-                                </a>
-                            );
-                        })
+                        data.policies.map(policy =>
+                            <a href={policy.link}>
+                                <button>
+                                    <p>{policy.text}</p>
+                                    <img src={arrow} />
+                                </button>
+                            </a>
+                        )
                     }
                 </div>
             </div>
