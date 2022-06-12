e009fb10dd6df1b54f04cfd82d309c60089d422f
VisShon <vishnu20414@iiitd.ac.in>
2022-06-12 22:15:03 +0530

    navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,10 @@ import NotFound from './pages/Notfound';
 import Faculty from './pages/Faculty';
 import Academics from './pages/Academics';
 import Research from './pages/ResearchLabs';
+import Teaching from './pages/TeachingLabs';
+import ResearchProj from './pages/ResearchProj';
+import ResearchPub from './pages/ResearchPub';
+import Contact from './pages/Contactus';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,9 +21,25 @@ root.render(
     <Routes>
       <Route path="/" element={<Landing/>} />
       <Route path="/CommingSoon" element={<CommingSoon/>}/>
+
+
       <Route path="/Faculty" element={<Faculty/>}/>
-      <Route path="/Research" element={<Research/>}/>
+      <Route path="/Staff" element={<Faculty/>}/>
+      <Route path="/Students" element={<Faculty/>}/>
+
+
+      <Route path="/ResearchLabs" element={<Research/>}/>
+      <Route path="/TeachingLabs" element={<Teaching/>}/>
+      <Route path="/ResearchProjects" element={<ResearchProj/>}/>
+      <Route path="/ResearchPub" element={<ResearchPub/>}/>
+
+
       <Route path="/Academics" element={<Academics/>}/>
+      <Route path="/PHD" element={<Academics/>}/>
+      <Route path="/StudentConduct" element={<Academics/>}/>
+
+      <Route path="/Contactus" element={<Contact/>}/>
+
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   </BrowserRouter>
