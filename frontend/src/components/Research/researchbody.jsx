7352f5bbda9b22968c2000e6fd5acfdea0972d31
VisShon <vishnu20414@iiitd.ac.in>
2022-06-16 23:27:15 +0530

    updates

--- frontend/src/components/Research/researchbody.jsx
+++ frontend/src/components/Research/researchbody.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../styles/research.css';
-
-
-function researchbody({data}) {
+function Researchbody({data}) {
+  const[hoverState,setHoverState] = useState(false)
+  const onHoverHandler=()=>{
+    setHoverState(true);
+  }
+  const leaveHandler=()=>{
+    setHoverState(false);
+  }
   if(data){
     return (
       <div class='body'>
@@ -11,7 +16,7 @@ function researchbody({data}) {
               <h2 id="title" className="font-Archivo text-[3.5rem] font-bold" style={{color: data.color}}>{data.title}</h2>
               <h id="info" className="font-Archivo text-[1.5rem] font-semibold text-[#464646]">{data.subtitle}</h>
               <p id="description" className="font-Maven text-[1.2vw] text-[#464646] w-[80%]">{data.description}</p>
-              <a id="btn" style={{backgroundColor: data.color}} className="font-Archivo font-semibold text-[#ffffff] w-[10%] text-[1vw] " href={data.link}>{data.buttonText}</a>
+              <a id="btn" style={hoverState?{backgroundColor:'#f1f1f1',color:data.color}:{backgroundColor: data.color,color:'#f1f1f1'}} onMouseOver={onHoverHandler} onMouseOut={leaveHandler} className="font-Archivo font-semibold w-[10%] text-[1vw] " href={data.link}>{data.buttonText}</a>
           </div>
         </div>
       </div>
@@ -20,4 +25,4 @@ function researchbody({data}) {
 
 }
 
-export default researchbody;
+export default Researchbody;
