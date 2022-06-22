c5fcf28987c55925de8b5bdea31be6c40515fbf9
Ambujbhaskar <bhaskarambuj7@gmail.com>
2022-06-22 17:58:57 +0530

    fixed subject buttons not showing

--- frontend/src/components/Academics/section5.jsx
+++ frontend/src/components/Academics/section5.jsx
@@ -9,11 +9,10 @@ export default function Section5({data}){
             <div className="section5Container">
                 <div className="subjects">
                     {
-                        data.subjects.map((subj) => {
-                             (
+                        data.subjects.map((subj) => (
                                 <Subject subjCode={subj.code} subjName={subj.text} selectedSub={sub} setSelectedSub={setSub} courses={data.courseList} setCourses={setCourses}/>
                             )
-                        })
+                        )
                     }
                 </div>
                 <div className="courses">
