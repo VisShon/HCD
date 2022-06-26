92fe835d6904159142fc6675cd4353459cea554c
VisShon <vishnu20414@iiitd.ac.in>
2022-06-26 13:21:07 +0530

    backend updated

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 3000;
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('/*',  (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+app.listen(port,() =>{console.log(`listening on port${port}`)});
