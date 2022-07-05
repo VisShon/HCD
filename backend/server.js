2ed124b17652dc3a9607da10c694ca2e1b44fcc3
VisShon <vishnu20414@iiitd.ac.in>
2022-07-05 20:08:37 +0530

    netlify

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('/*',  (req, res) => {
+app.get('*',  (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 app.listen(port,() =>{console.log(`listening on port${port}`)});
