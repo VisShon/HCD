0e439bab89c2e315276460afdb671742bbcbea16
Ayush Singal <ayushsingal22@gmail.com>
2022-07-05 19:40:23 +0530

    done contactus page and people pages

--- frontend/src/components/People/facultyCard.jsx
+++ frontend/src/components/People/facultyCard.jsx
@@ -3,9 +3,9 @@ import '../../styles/faculty.css';
 function FacultyCard(data) {
   return (
     <div className={"facultyCard"+(data.right?" rightAnimate":"")+(data.left?" leftAnimate":"")}>
-        <img src={require("../../assets/profsImg/"+data.image)} className="rounded-full h-[5vw] w-[5vw] object-cover mb-[1rem]"/>
-        <span className="font-Maven w-[60%] text-center"><b>{data.Name}</b></span>
-        <span className="font-Maven  w-[60%] text-center">{data.Description}</span>
+        <img src={require("../../assets/profsImg/"+data.image)} className="rounded-full h-[5vw] w-[5vw] object-cover mb-[1rem] imi"/>
+        <span className="font-Maven w-[60%] text-center texti"><b>{data.Name}</b></span>
+        <span className="font-Maven  w-[60%] text-center texti">{data.Description}</span>
     </div>
   )
 }
--- frontend/src/pages/Faculty.jsx
+++ frontend/src/pages/Faculty.jsx
@@ -13,9 +13,9 @@ function Faculty(){
                     <div className="facultyImage absolute bottom-0 w-[47vw] left-0">
                         <img src={FacultyImg} className=" object-fill"/>
                     </div>
-                    <span className='font-Archivo font-[600] text-[8vw] text-[#f1f1f1]'>FACULTY</span>
+                    <span className='font-Archivo font-[600] text-[8vw] text-[#f1f1f1] z-10'>FACULTY</span>
                 </div>
-                <div className="flex justify-between w-[60%]">
+                <div className="flex justify-between w-[60%] datai">
                     <div id="profCardContainer" className='ml-10'>
                         {data.map((items)=>(
                             (<FacultyCard left={true} image={items.image} Name={items.name} Description={items.description} key={items.id}/>)
