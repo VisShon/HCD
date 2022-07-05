1d3fbb7b8d2b2a6fa6fa5e9af098b4f89a1a817b
VisShon <vishnu20414@iiitd.ac.in>
2022-07-05 22:59:15 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const port = 3000;
+const dotenv = require('dotenv').config();
+const port = process.env.PORT||3000;
 const path = require('path');
 
 const app = express();
