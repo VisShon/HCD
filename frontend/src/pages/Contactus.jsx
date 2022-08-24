53c197e9564d5671404cc77a5ed610f4dd472a9d
EshanShah <eshangshah@gmail.com>
2022-08-24 13:23:50 +0530

    editedGallery

--- frontend/src/pages/Contactus.jsx
+++ frontend/src/pages/Contactus.jsx
@@ -5,6 +5,7 @@ import tweet from '../assets/tweet.png';
 import insta from '../assets/insta.png';
 import facebook from '../assets/facebook.png'
 import { Map, Marker } from "pigeon-maps"
+import linkedin from '../assets/linkedin.png';
 
 function Contactus() {
   return (
@@ -23,8 +24,10 @@ function Contactus() {
           </div>
 
           <div id="Address" className="Card ">
+          <a href="https://www.iiitd.ac.in/">
               <p><b>Address</b></p>
-              <p className="text-[#414141]">Okhla Industrial Estate, Phase III, near Govind Puri Metro Station, New Delhi, Delhi 110020</p>
+            </a>  
+              <p className="text-[#414141]">Indraprastha Institute of Information Technology Delhi Okhla Industrial Estate, Phase III (Near Govind Puri Metro Station) New Delhi, India - 110020</p>
           </div>
 
           <div id="contacts" className="Card">
@@ -33,14 +36,17 @@ function Contactus() {
               <a className="text-[#414141]" href="tel:+91-11-26907588">+91-11-26907588</a>
               <p className="mt-4"><b>Socials</b></p>
               <span className='flex my-2 justify-between w-[40%]'>
-                <a href="https://www.facebook.com/IIITDelhi/">
-                  <img className='hover:scale-125' src={facebook}/>
+                <a href="https://www.facebook.com/hcdiiitd">
+                  <img className='hover:scale-125' src={facebook}  />
                 </a>
-                <a href="https://www.instagram.com/iiit.delhi" >
+                <a href="https://www.instagram.com/hcdiiitd/" >
                   <img className='hover:scale-125' src={insta}/> 
                 </a>
-                <a href="https://twitter.com/IIITDelhi">
+                <a href="https://twitter.com/hcdiiitd">
                   <img className='hover:scale-125' src={tweet}/> 
+                </a>
+                <a href="https://www.linkedin.com/company/hcdiiitd/mycompany/">
+                  <img className='hover:scale-125' src={linkedin}/> 
                 </a>
               </span>
           </div>
