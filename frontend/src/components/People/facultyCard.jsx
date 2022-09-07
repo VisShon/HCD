54387df3350b5697bba96b6b075da9b50bbf68d5
EshanShah <eshangshah@gmail.com>
2022-09-07 14:54:46 +0530

    facultyAdded

--- frontend/src/components/People/facultyCard.jsx
+++ frontend/src/components/People/facultyCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function FacultyCard(data) {
   return (
-    <div className="group text-center text-[#616161] mx-10">
-        <img src={require("../../assets/profsImg/"+data.image)}/>
+    <div className="group text-center text-[#616161] mx-10  flex flex-col items-center">
+        <img className="relative" src={require("../../assets/profsImg/"+data.image)}/>
         <p className="font-[600] group-hover:text-[#39AEA8]">{data.name}</p>
-        <p>{data.description}</p>
+        <p className="w-90 ">{data.description}</p>
     </div>
   )
 }
--- frontend/src/pages/Advisory.jsx
+++ frontend/src/pages/Advisory.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/faculty.css';
 import NavBar from '../components/navbar';
 import FacultyCard  from '../components/People/facultyCard';
-import data from '../data/staffPageData.json'
+import data from '../data/advisory.json'
 import Footer from '../components/footer';
 function Advisory() {
   return (
@@ -12,7 +12,7 @@ function Advisory() {
                 <span id="heading" className='font-Archivo font-[200] text-[10vw] small:w-[100%]'>ADVISORY</span>
         </div>
         <div id="ContentFaculty" className="font-Maven text-[#000000] text-[1rem] flex justify-center">
-            <div className="facultyContainer">
+            <div className="advisory">
                     {data.map((prof,index)=>(
                     <FacultyCard image={prof.image} name={prof.name} description={prof.description}/>
                     ))}
